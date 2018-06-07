2839c1fa317464b01e76d11eb51ef2cf1e6ffe91
jmartinez888 <34007650+jmartinez888@users.noreply.github.com>
2018-06-07 02:15:40 -0500

    Correo amazon mailer

--- application/Correo.php
+++ application/Correo.php
@@ -4,25 +4,49 @@ class Correo
 {
     private $mail;
     public function __construct() {
+        // $this->mail = new PHPMailer();
+        // $this->mail->IsSMTP();
+        // // $this->mail->CharSet="UTF-8";
+        // // Activamos / Desactivamos el "debug" de SMTP 
+        // // 0 = Apagado 
+        // // 1 = Mensaje de Cliente 
+        // // 2 = Mensaje de Cliente y Servidor 
+        // $this->mail->SMTPDebug = 0;
+        // // Log del debug SMTP en formato HTML 
+        // $this->mail->Debugoutput = 'html';
+        // $this->mail->Host = 'email-smtp.us-west-2.amazonaws.com';
+        // // $this->mail->Host = 'smtp.gmail.com';
+        // $this->mail->SMTPAuth = true;
+        // $this->mail->Username = 'AKIAJ4CISRDL4GEUDMXA';
+        // // $this->mail->Username = 'pruebanombrea@gmail.com';
+        // $this->mail->Password = 'AqP5InJ8BqxcNu9yMvO02yOwiwpbr61aaRbV0h99PXxX';
+        // // $this->mail->Password = '1357902468@pna';
+        // $this->mail->SMTPSecure = 'tls'; //tls or ssl
+        // $this->mail->Port = 587; //587 or 465
+
+        // Instantiate a new PHPMailer 
         $this->mail = new PHPMailer();
-        $this->mail->IsSMTP();
-        // $this->mail->CharSet="UTF-8";
-        // Activamos / Desactivamos el "debug" de SMTP 
-        // 0 = Apagado 
-        // 1 = Mensaje de Cliente 
-        // 2 = Mensaje de Cliente y Servidor 
-        $this->mail->SMTPDebug = 0;
-        // Log del debug SMTP en formato HTML 
-        $this->mail->Debugoutput = 'html';
+
+        // Tell PHPMailer to use SMTP
+        $this->mail->isSMTP();
+        // Specify a configuration set. If you do not want to use a configuration
+        // set, comment or remove the next line.
+        $this->mail->addCustomHeader('X-SES-CONFIGURATION-SET', 'ConfigSet');         
+        // If you're using Amazon SES in a region other than US West (Oregon), 
+        // replace email-smtp.us-west-2.amazonaws.com with the Amazon SES SMTP  
+        // endpoint in the appropriate region.
         $this->mail->Host = 'email-smtp.us-west-2.amazonaws.com';
-        // $this->mail->Host = 'smtp.gmail.com';
+        // Tells PHPMailer to use SMTP authentication
         $this->mail->SMTPAuth = true;
+        // Replace smtp_username with your Amazon SES SMTP user name.
         $this->mail->Username = 'AKIAJ4CISRDL4GEUDMXA';
-        // $this->mail->Username = 'pruebanombrea@gmail.com';
+        // Replace smtp_password with your Amazon SES SMTP password.
         $this->mail->Password = 'AqP5InJ8BqxcNu9yMvO02yOwiwpbr61aaRbV0h99PXxX';
-        // $this->mail->Password = '1357902468@pna';
-        $this->mail->SMTPSecure = 'tls'; //tls or ssl
-        $this->mail->Port = 587; //587 or 465
+        // Enable TLS encryption over port 587
+        $this->mail->SMTPSecure = 'tls';
+        $this->mail->Port = 587;
+
+
     }
 
     public function enviar($forEmail, $forName, $Subject, $contenido, $fromName = "Proyecto PRIC"){
