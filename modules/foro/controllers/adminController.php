8a4afe5daf5f55a01698c9cfe782fb3d87bd57b5
Julio <juliocesarriosgarcia@gmail.com>
2018-06-28 18:05:47 -0500

    f

--- modules/foro/controllers/adminController.php
+++ modules/foro/controllers/adminController.php
@@ -615,10 +615,10 @@ class adminController extends foroController
         $titulo = "PRUEBA DE TITULO";
         //cabecera
 
-        $headers = "MIME-Version: 1.0".'\r\n'; 
-        $headers .= "Content-type: text/html; charset=iso-8859-1".'\r\n'; 
+        $headers = "MIME-Version: 1.0\r\n"; 
+        $headers .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
         //dirección del remitente 
-        $headers .= "From: Geeky Theory < ".$correo2." >".'\r\n'; 
+        $headers .= "From: Geeky Theory < ".$correo2." >\r\n"; 
         // //dirección del remitente 
         // $headers .= "From: Geeky Theory < ".$email." >".'\r\n';
         //Enviamos el mensaje a tu_dirección_email 
