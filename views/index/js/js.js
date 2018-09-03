96e0f549fb8440f1b3535b3ed7dbcaedbbee1808
jmartinez888 <34007650+jmartinez888@users.noreply.github.com>
2018-09-03 11:13:14 -0500

    Info_Map

--- views/index/js/js.js
+++ views/index/js/js.js
@@ -462,7 +462,7 @@ var cssInfoWindows = function(infowindowLocal){
       var iwCloseBtn = iwOuter.next();
 
       // Apply the desired effect to the close button
-      iwCloseBtn.css({width: '25px', height: '25px', opacity: '1', right: '40px', top: '2px', border: '6px solid #5aa609', 'border-radius': '15px', 'box-shadow': '0 0px 5px rgba(24, 83, 14, 0.98)'});
+      iwCloseBtn.css({width: '25px', height: '25px', opacity: '1', right: '0px', top: '2px', border: '6px solid #5aa609', 'border-radius': '15px', 'box-shadow': '0 0px 5px rgba(24, 83, 14, 0.98)'});
 
       // If the content of infowindow not exceed the set maximum height, then the gradient is removed.
       if($('.iw-content').height() < 140){
