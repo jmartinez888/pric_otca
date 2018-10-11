b3bf17d35d513caa1dbfe68974d6041d7643e36d
Jaen Pierre <jaen.qn@gmail.com>
2018-10-10 22:16:47 -0500

    Update modules/usuarios/class/Usuario.php

---
+++ modules/usuarios/class/Usuario.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class Usuario extends Eloquent
+{
+  protected $table = 'usuario';
+  protected $primaryKey = 'Usu_IdUsuario';
+  public $timestamps = false;
+
+  // public static function activos () {
+  // 	return self::where('Idi_Estado', 1)->get();
+  // }
+  // public function scopeActivos () {
+
+  // }
+
+  public function scopebuscar ($query, $target) {
+    return $query->where('Usu_Nombre', 'like', "%$target%")
+            ->orWhere('Usu_Apellidos', 'like', "%$target%")
+            ->orWhere('Usu_Usuario', 'like', "%$target%");
+  }
+
+  public function scopepara_mencion ($query) {
+    return $query->select(['Usu_IdUsuario', 'Usu_Nombre', 'Usu_Apellidos']);
+  }
+}
