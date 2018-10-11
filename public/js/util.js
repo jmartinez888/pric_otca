ec7d4c142a9498db9cf6df662afdb15b761922cb
Jaen Pierre <jaen.qn@gmail.com>
2018-10-10 22:27:11 -0500

    public/js
    axios, datatables, moment, mustache, utils(modificado), vuejs

--- public/js/util.js
+++ public/js/util.js
@@ -1,31 +1,41 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
+var msg = {
+    success: function (text, stringify = false) {
+        if (text)
+            mensaje([['ok', stringify ? JSON.stringify(text) : text]])
+    },
+    error: function (text, stringify = false) {
+        if (text)
+            mensaje([['error', stringify ? JSON.stringify(text) : text]])
+    }
+}
 
 function mensaje(resultado) {
-
+    console.log(resultado)
     var error = "<div class='alert  alert-error '><a class='close' data-dismiss='alert'>x</a><label></label></div>"
     var ok = "<div class='alert alert-success '><a class='close' data-dismiss='alert'>x</a><label></label></div>"
     $("#_mensaje").removeClass("hide");
-    
+
     if ($.isArray(resultado) && (!$.isEmptyObject(resultado))) {
         $.each(resultado, function(key, value) {
 
             if ($.isArray(value) && (!$.isEmptyObject(value))) {
                 if (value[0] == "error") {
-                    var div=$(error)              
+                    var div=$(error)
                     div.find('label').html(value[1]);
                     $("#_mensaje").append(div);
                 } else if (value[0] == "ok") {
-                    var div=$(ok)              
+                    var div=$(ok)
                     div.find('label').html(value[1]);
                      $("#_mensaje").append(div);
                 }
             } else {
-                var div=$(error)           
+                var div=$(error)
                 if (value == "") {
                      div.find('label').html("Ocurrio un error vuelva a intentarlo luego: Error: " + value);
                 } else {
