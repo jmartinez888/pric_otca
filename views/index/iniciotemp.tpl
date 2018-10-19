9d776e519d7db43bfd95bd595281d5788a80e52e
Jaen Pierre <jaen.qn@gmail.com>
2018-10-18 19:24:09 -0500

    update
    complemento del cambio en el inicio de la página

---
+++ views/index/iniciotemp.tpl
@@ -0,0 +1,217 @@
+{extends 'template.tpl'}
+{block 'css'}
+<style type="text/css" media="screen">
+    .mainstage .main_story .transition_layer {
+        background-repeat: no-repeat !important;
+        background-size: 100%;
+    }
+</style>
+{/block}
+{block 'contenido'}
+<div id="inicio">
+
+    <!-- NOTICIAS -->
+    <div id="TB" class="jsoft-bloque padding-t-10">
+        <div id="page col-md-12">
+            <div id="banner col-md-12">
+                {* <objtemplate objtemplate_id="517" objtemplate_filename="getunik.pandaorg3.h-spotlight"></objtemplate> *}
+
+                {* <objtemplate objtemplate_id="517" objtemplate_filename="getunik.pandaorg3.h-spotlight"> *}
+                    <div class="row">
+                        <div class="main-column">
+                            <div class="container col-sm-12">
+                                <div class="mainstage col-sm-12">
+                                    {* <a class="main_story" href="http://www.otca-oficial.info/news/details/341" style="background-image: url({$_layoutParams.ruta_img}slider_noticias/noticia1.JPG); background-color: #000000;">
+                                        <p class="alt">Cinco de las siete especies de tortugas marinas que existen en la costa peruana y todas ...</p>
+                                    </a> *}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                {* </objtemplate> *}
+            </div>
+        </div>
+    </div>
+
+
+                        <hr>
+    <!-- SERVICIOS Y MAPA DE DATOS Y CIFRAS-->
+    <div class="row padding-10">
+        <div class="col col-md-8 col-sm-12 col-xs-12 ">
+            <div class="cont-recursos row">
+                <div class="tit-float-left-top">
+                    Servicios PRICasd
+
+                </div>
+
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img  src="{$_layoutParams.ruta_img}m_cursos.png">
+                        <div class="caption">
+                        <h3>Cursos</h3>
+                        <p style="text-align: justify;">Actualmente la plataforma PRIC ofrece una gran variedad de cursos en modalidades mooc y e-learning, tanto de larga duración, Másters y Expertos, como de Formación Continua y Especializada ...</p>
+                        <p><a href="{$_layoutParams.root}elearning" class="btn-jsoft btn btn-primary" role="button">ver más</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img  src="{$_layoutParams.ruta_img}m_discusiones.png">
+                        <div class="caption">
+                        <h3>Discusiones</h3>
+                        <p style="text-align: justify;">Discusión en línea asincrónico donde las personas publican mensajes alrededor de un tema, creando de esta forma un hilo de conversación jerárquico; los foros o discusiones, a su vez, ...</p>
+                        <p><a href="{$_layoutParams.root}foro" class="btn-jsoft btn btn-primary" role="button">ver más</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img  src="{$_layoutParams.ruta_img}m_documentos.png">
+                        <div class="caption">
+                        <h3>Documentos</h3>
+                        <p style="text-align: justify;">En la actualidad lo habitual es almacenar toda la información de manera digital con el software pertinente y este es necesario que, entre otras cosas, ofrezca personalización e interoperatividad.</p>
+                        <p><a href="{$_layoutParams.root}dublincore/documentos" class="btn-jsoft btn btn-primary" role="button">ver más</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- EVENTOS E INTERESES -->
+        <div class="col col-md-4 col-sm-12 col-xs-12 back-color-white" style="border-left: 1px solid #ebeaea; padding-left: 10px;">
+            <ul class="nav nav-tabs jsoft-tabs bg-verde">
+                <li class="active" ><a class=""  data-toggle="tab" href="#evento_interes">Eventos de Interés</a></li>
+                <li><a data-toggle="tab" href="#dato_interes">Datos de Interés</a></li>
+            </ul>
+            <div class="tab-content">
+                <div id="evento_interes" class="tab-pane fade  active in scroll">
+                    {foreach $interes_evento as $item}
+                        <interes id="{$item->ODif_IdDifusion}"></interes>
+                    {/foreach}
+                    <a href="#" class="col-md-12 col-sm-12 col-xs-12 mas-jsoft">VER MÁS</a>
+                </div>
+                <div id="dato_interes" class="tab-pane fade scroll">
+                    {foreach $interes_datos as $item}
+                        <interes id="{$item->ODif_IdDifusion}"></interes>
+                    {/foreach}
+                    <a href="#" class="col-md-12 col-sm-12 col-xs-12 mas-jsoft">VER MÁS</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col col-md-12 col-sm-12 col-xs-12">
+           <hr>
+        </div>
+        <!-- EVENTOS E INTERESES -->
+
+        <div class="col col-md-12 col-sm-12 col-xs-12">
+            <div class=" margin-0 " style="background: url({$_layoutParams.ruta_img}frontend/bg-contador.png); heigh:60vh">
+                <div class="tit-float-left-top" style="margin-top: 17px;">
+                Datos y cifras
+                </div>
+
+                    <div id="map" style="height: 60vh;width:100%"></div>
+                 <a href="#" class="mas_cifras"> +</a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+{/block}
+
+{block 'template'}
+
+    {* <template id="interes">
+        <a target="_blank" :href="'{$_layoutParams.root}difusion/contenido/' + id" class="col-md-12 col-sm-12 col-xs-12  link-tabs-jsoft">
+            <div class="col-md-4 col-sm-4 col-xs-4  py-3 px-2">
+                <img :src="difusion.ODif_Image" class="w-100">
+            </div>
+            <div class="col-md-8 col-sm-8 col-xs-8">
+                <div class="card-block ">
+                    <h3 class="card-title" style="font-size: 15px;">{literal}{{ difusion.ODif_Titulo }}{/literal}</h3>
+                    <p class="card-text" style="font-size: 12px;"><i class="fa fa-calendar"></i> Publicado: {literal}{{ moment().format('YYYY-MM-DD') }}{/literal}</p>
+                </div>
+            </div>
+        </a>
+    </template> *}
+{/block}
+{block 'js'}
+    <script src="{BASE_URL}public/js/axios/dist/axios.min.js" type="text/javascript"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBM7aMHbWEPvofhwPQuKPnijDmQ0_AAkrI" type="text/javascript"></script>
+    <script src="{BASE_URL}public/js/moment/moment.js" type="text/javascript"></script>
+    <script src="{BASE_URL}public/js/vuejs/vue.min.js" type="text/javascript"></script>
+    <script src="{$_layoutParams.ruta_js}global.js" type="text/javascript"></script>
+    <script>
+        var banners = {$banners};
+    </script>
+    <script src="{$_layoutParams.root}difusion/contenido/vcomponent/interes"></script>
+    <script src="{BASE_URL}views/index/js/inicio.js" type="text/javascript"></script>
+
+    <!-- <script type="text/javascript">
+        var jQuery_1_5_7 = $.noConflict(true);
+    </script> -->
+
+    <script type="text/javascript">
+        /* <![CDATA[ */
+        /* this JSON copied from current, live site for the sake of testing. In production it would be populated dynamically.*/
+        // $('.mainstage').wwfMainstage([{
+
+        //     'backgroundImage': '{$_layoutParams.ruta_img}slider_noticias/noticia1.JPG',
+        //     'thumbnail': '{$_layoutParams.ruta_img}slider_noticias/thumbnail/noticia1.JPG',
+        //     'credit':'©  OTCA | foto ceremonia: Marcos Corrêa/PR',
+        //     'categoria': 'NOTICIA',
+        //     'headline': 'Potencialidades - Plantas para uso medicinal ou cosmético.',
+        //     // 'headline2': 'Urgen compromisos para su conservación',
+
+        //     'href': '{$_layoutParams.root}noticias/index/index/341',
+        //     'storyTitle': 'Potencialidades - Plantas para uso medicinal ou cosmético ...'
+
+
+        // },
+        // {
+
+        //     'backgroundImage': '{$_layoutParams.ruta_img}slider_noticias/noticia2.jpg',
+        //     'thumbnail': '{$_layoutParams.ruta_img}slider_noticias/thumbnail/noticia2.jpg',
+        //     'credit':'© OTCA',
+        //     'categoria': 'NOTICIA',
+        //     'headline': 'La selva amazónica: formada hace 20 millones de años',
+        //     // 'headline2': 'WWF lanza Premio Periodístico en el Perú',
+
+        //     //'href': 'http://www.otca-oficial.info/news/details/333',
+        //     'href': '{$_layoutParams.root}noticias/index/index/333',
+        //     'storyTitle': 'La selva amazónica: formada hace 20 millones de años al surgir de los andes...'
+        // },
+        // {
+
+        //     'backgroundImage': '{$_layoutParams.ruta_img}slider_noticias/noticia3.jpg',
+        //     'thumbnail': '{$_layoutParams.ruta_img}slider_noticias/thumbnail/noticia3.jpg',
+        //     'credit':'© OTCA',
+        //     'categoria': 'INVESTIGACIÓN',
+        //     'headline': 'La atlántida amazónica.',
+        //     // 'headline2': 'Iniciativa logra recuperar y reforestar suelos degradados',
+
+        //     //'href': 'http://www.otca-oficial.info/news/details/338',
+        //     'href': '{$_layoutParams.root}noticias/index/index/338',
+        //     'storyTitle': 'La atlántida amazónica, nuevas pruebas sedimentarias sufieren que, hace millones de años, una parte de ...'
+        // },
+        // {
+
+        //     'backgroundImage': '{$_layoutParams.ruta_img}slider_noticias/noticia4.jpg',
+        //     'thumbnail': '{$_layoutParams.ruta_img}slider_noticias/thumbnail/noticia4.jpg',
+        //     'credit':'© CAF | OTCA',
+        //     'categoria': 'INVESTIGACIÓN',
+        //     'headline': '¿Cuándo se formó el río Amazonas?',
+        //     // 'headline2': 'Promoviendo su conservación en el litoral peruano',
+
+        //     //'href': 'http://www.otca-oficial.info/news/details/328',
+        //     'href': '{$_layoutParams.root}noticias/index/index/328',
+        //     'storyTitle': 'Un nuevo estudio data el nacimiento del río más caudaloso del mundo en más de 9 millones de años ...'
+        // },
+
+
+
+        // ]);
+
+    </script>
+{/block}
---
+++ views/index/js/inicio.js
@@ -0,0 +1,427 @@
+// const interes = {
+// 	props: ['id'],
+// 	data: function () {
+// 		return {
+// 			difusion: {
+// 				ODif_Image: ''
+// 			}
+// 		}
+// 	},
+// 	template: '#interes',
+// 	created () {
+// 		axios.get(_root_lang + 'difusion/contenido/' + this.id + '?type=json').then(res => {
+// 			console.log(res.data)
+// 			this.difusion = res.data.data
+// 		})
+// 		console.log(this.id)
+// 	}
+// }
+
+
+new Vue({
+	el: '#inicio',
+	// components: {
+	// 	interes
+	// },
+	methods: {
+		cssInfoWindows: function(infowindowLocal){
+		    google.maps.event.addListener(infowindowLocal, 'domready', function() {
+
+		      // Reference to the DIV that wraps the bottom of infowindow
+		      var iwOuter = $('.gm-style-iw');
+
+		      iwOuter.css({'background-color': '#fff', 'box-shadow': '0 1px 6px rgba(24, 83, 14, 0.98)', 'border': '1px solid 3c763d', 'border-radius': '2px 2px 10px 10px'});
+		      /* Since this div is in a position prior to .gm-div style-iw.
+		       * We use jQuery and create a iwBackground variable,
+		       * and took advantage of the existing reference .gm-style-iw for the previous div with .prev().
+		      */
+		      var iwBackground = iwOuter.prev();
+
+		      // Removes background shadow DIV
+		      iwBackground.children(':nth-child(2)').css({'display' : 'none'});
+
+		      // Removes white background DIV
+		      iwBackground.children(':nth-child(4)').css({'display' : 'none'});
+
+		      // Moves the infowindow 115px to the right.
+		      iwOuter.parent().parent().css({left: '0px'});
+
+		      // Moves the shadow of the arrow 76px to the left margin.
+		      iwBackground.children(':nth-child(1)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
+
+		      // Moves the arrow 76px to the left margin.
+		      iwBackground.children(':nth-child(3)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
+
+		      // Changes the desired tail shadow color.
+		      iwBackground.children(':nth-child(3)').find('div').children().css({'box-shadow': '0 1px 6px rgba(24, 83, 14, 0.98)', 'z-index' : '1'});
+
+		      // Reference to the div that groups the close button elements.
+		      var iwCloseBtn = iwOuter.next();
+
+		      // Apply the desired effect to the close button
+		      iwCloseBtn.css({width: '25px', height: '25px', opacity: '1', right: '0px', top: '2px', border: '6px solid #5aa609', 'border-radius': '15px', 'box-shadow': '0 0px 5px rgba(24, 83, 14, 0.98)'});
+
+		      // If the content of infowindow not exceed the set maximum height, then the gradient is removed.
+		      if($('.iw-content').height() < 140){
+		        $('.iw-bottom-gradient').css({display: 'none'});
+		      }
+
+		      // The API automatically applies 0.7 opacity to the button after the mouseout event. This function reverses this event to the desired value.
+		      iwCloseBtn.mouseout(function(){
+		        $(this).css({opacity: '1'});
+		      });
+		    });
+		},
+		animacionMarker : function(markerAnimar){
+		    if (markerAnimar.getAnimation() !== null) {
+		        markerAnimar.setAnimation(null);
+		    } else {
+		        markerAnimar.setAnimation(google.maps.Animation.BOUNCE);
+		    }
+		},
+		initMap : function () {
+			var _this = this;
+			var myLatlng = new google.maps.LatLng(-11.5106611,-60.8686626);
+			var map = new google.maps.Map(document.getElementById('map'), {
+		    center: {lat:-11.413054, lng: -60.443913},
+		    zoom: 3,
+		    disableDefaultUI: true,
+		    styles:[
+		      {
+		        "elementType": "labels",
+		        "stylers": [
+		          {
+		            "visibility": "off"
+		          }
+		        ]
+		      },
+		      {
+		        "elementType": "labels.icon",
+		        "stylers": [
+		          {
+		            "visibility": "off"
+		          }
+		        ]
+		      },
+		      {
+		        "elementType": "labels.text.fill",
+		        "stylers": [
+		          {
+		            "color": "#616161"
+		          }
+		        ]
+		      },
+		      {
+		        "elementType": "labels.text.stroke",
+		        "stylers": [
+		          {
+		            "color": "#f5f5f5"
+		          }
+		        ]
+		      },
+		      {
+		        "featureType": "administrative",
+		        "elementType": "geometry",
+		        "stylers": [
+		          {
+		            "visibility": "off"
+		          }
+		        ]
+		      },
+		      {
+		        "featureType": "administrative.land_parcel",
+		        "elementType": "labels.text.fill",
+		        "stylers": [
+		          {
+		            "color": "#bdbdbd"
+		          }
+		        ]
+		      },
+		      {
+		        "featureType": "administrative.neighborhood",
+		        "stylers": [
+		          {
+		            "visibility": "off"
+		          }
+		        ]
+		      },
+		      {
+		        "featureType": "poi",
+		        "stylers": [
+		          {
+		            "visibility": "off"
+		          }
+		        ]
+		      },
+		      {
+		        "featureType": "poi",
+		        "elementType": "geometry",
+		        "stylers": [
+		          {
+		            "color": "#eeeeee"
+		          }
+		        ]
+		      },
+		      {
+		        "featureType": "poi",
+		        "elementType": "labels.text.fill",
+		        "stylers": [
+		          {
+		            "color": "#757575"
+		          }
+		        ]
+		      },
+		      {
+		        "featureType": "poi.park",
+		        "elementType": "geometry",
+		        "stylers": [
+		          {
+		            "color": "#e5e5e5"
+		          }
+		        ]
+		      },
+		      {
+		        "featureType": "poi.park",
+		        "elementType": "labels.text.fill",
+		        "stylers": [
+		          {
+		            "color": "#9e9e9e"
+		          }
+		        ]
+		      },
+		      {
+		        "featureType": "road",
+		        "stylers": [
+		          {
+		            "visibility": "off"
+		          }
+		        ]
+		      },
+		      {
+		        "featureType": "road",
+		        "elementType": "geometry",
+		        "stylers": [
+		          {
+		            "color": "#ffffff"
+		          }
+		        ]
+		      },
+		      {
+		        "featureType": "road",
+		        "elementType": "labels.icon",
+		        "stylers": [
+		          {
+		            "visibility": "off"
+		          }
+		        ]
+		      },
+		      {
+		        "featureType": "road.arterial",
+		        "stylers": [
+		          {
+		            "visibility": "off"
+		          }
+		        ]
+		      },
+		      {
+		        "featureType": "road.arterial",
+		        "elementType": "labels.text.fill",
+		        "stylers": [
+		          {
+		            "color": "#757575"
+		          }
+		        ]
+		      },
+		      {
+		        "featureType": "road.highway",
+		        "elementType": "geometry",
+		        "stylers": [
+		          {
+		            "color": "#dadada"
+		          }
+		        ]
+		      },
+		      {
+		        "featureType": "road.highway",
+		        "elementType": "labels",
+		        "stylers": [
+		          {
+		            "visibility": "off"
+		          }
+		        ]
+		      },
+		      {
+		        "featureType": "road.highway",
+		        "elementType": "labels.text.fill",
+		        "stylers": [
+		          {
+		            "color": "#616161"
+		          }
+		        ]
+		      },
+		      {
+		        "featureType": "road.local",
+		        "stylers": [
+		          {
+		            "visibility": "off"
+		          }
+		        ]
+		      },
+		      {
+		        "featureType": "road.local",
+		        "elementType": "labels.text.fill",
+		        "stylers": [
+		          {
+		            "color": "#9e9e9e"
+		          }
+		        ]
+		      },
+		      {
+		        "featureType": "transit",
+		        "stylers": [
+		          {
+		            "visibility": "off"
+		          }
+		        ]
+		      },
+		      {
+		        "featureType": "transit.line",
+		        "elementType": "geometry",
+		        "stylers": [
+		          {
+		            "color": "#e5e5e5"
+		          }
+		        ]
+		      },
+		      {
+		        "featureType": "transit.station",
+		        "elementType": "geometry",
+		        "stylers": [
+		          {
+		            "color": "#eeeeee"
+		          }
+		        ]
+		      },
+		      {
+		        "featureType": "water",
+		        "elementType": "geometry",
+		        "stylers": [
+		          {
+		            "color": "#c9c9c9"
+		          }
+		        ]
+		      },
+		      {
+		        "featureType": "water",
+		        "elementType": "geometry.fill",
+		        "stylers": [
+		          {
+		            "color": "#28488d"
+		          },
+		          {
+		            "visibility": "on"
+		          }
+		        ]
+		      },
+		      {
+		        "featureType": "water",
+		        "elementType": "labels.text.fill",
+		        "stylers": [
+		          {
+		            "color": "#9e9e9e"
+		          }
+		        ]
+		      }
+		    ]
+	  	});
+
+			map.data.loadGeoJson(_root_ + 'public/img/Area_estudio_Amz.geojson');
+			map.data.setStyle({
+		    fillColor: 'green',
+		    strokeColor: '#ffffff',
+		    strokeWeight: 2
+		  });
+			var iconBase = _root_ + 'views/layout/frontend/img/frontend/';
+
+			axios.get(_root_lang + 'difusion/contenido/datos_cifras').then(res => {
+				console.log(res.data)
+
+				res.data.forEach(item => {
+					var contentString = `
+						<div id="iw-container">
+				    	<div class="iw-title">${item.indicador.OInd_Titulo}</div>
+				    	<div class="iw-content">
+				    		<div class="iw-subTitle">${item.ODii_Descripcion}<sup>2</sup> &nbsp;&nbsp;<a href="${_root_lang + 'difusion/contenido/' + item.difusion.ODif_IdDifusion}" title="Ver más" target="_blank" class="glyphicon glyphicon-share"></a>
+				    	</div>
+				    </div>`;
+
+				    var infowindow = new google.maps.InfoWindow({
+						  content: contentString
+						});
+
+						var marker = new google.maps.Marker({
+						    position: new google.maps.LatLng(item.ODii_PosLatitude, item.ODii_PosLongitude),
+						    title: item.indicador.OInd_Titulo,
+						    icon: iconBase + item.indicador.OInd_IconoPath
+						});
+
+						marker.setMap(map);
+						this.cssInfoWindows(infowindow);
+
+						marker.addListener('click', function(event) {
+						  // infowindow.open(map, marker);
+
+						    map.setZoom(4);
+						    map.setCenter(marker.getPosition());
+						    map.data.revertStyle();
+						    // map.data.overrideStyle({strokeWeight: 8});
+						    map.data.setStyle({
+						      fillColor: 'green',
+						      strokeColor: '#008000',
+						      strokeWeight: 4
+						    });
+						    // infowindow2.close();
+						    // infowindow3.close();
+						    // infowindow4.close();
+						    infowindow.open(map, marker);
+						    marker.setIcon(iconBase + 'ic-marker-1_hover.png');
+						    _this.animacionMarker(marker);
+						});
+						marker.addListener('mouseover', function(event) {
+						  // infowindow.open(map, marker);
+
+						    map.data.setStyle({
+						      fillColor: 'green',
+						      strokeColor: '#008000',
+						      strokeWeight: 2
+						    });
+						    marker.setIcon(iconBase + item.indicador.OInd_IconoPath);
+						    // marker.setIcon(iconBase + 'ic-marker-1_hover.png');
+						    _this.animacionMarker(marker);
+						});
+						marker.addListener('mouseout', function(event) {
+						  // infowindow.open(map, marker);
+
+						    map.data.setStyle({
+						      fillColor: 'green',
+						      strokeColor: '#efa30b',
+						      strokeWeight: 1
+						    });
+						    marker.setAnimation(null);
+						    // marker.setIcon(iconBase + 'ic-marker-1.png');
+						    marker.setIcon(iconBase + item.indicador.OInd_IconoPath);
+						});
+
+				})
+			})
+
+		}
+	},
+	created () {
+
+	},
+	mounted: function () {
+		$('.mainstage').wwfMainstage(banners);
+		this.initMap()
+	}
+});
