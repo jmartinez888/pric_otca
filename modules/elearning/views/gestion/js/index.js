205a58bb7b5edfd507591fea9b8ec93cf0795604
jmartinez888 <34007650+jmartinez888@users.noreply.github.com>
2019-01-30 06:43:34 -0500

    Update index.js

--- modules/elearning/views/gestion/js/index.js
+++ modules/elearning/views/gestion/js/index.js
@@ -5,6 +5,15 @@ $(document).ready(function(){
   }else{
     CargarPagina("gestion/_view_mis_cursos", {});
   }
+  $("#btn_nuevo_curso").click(function(){
+    try{
+      CargarPagina("gcurso/_view_mis_cursos", {}, false, $(this));
+    }catch(e){
+      if (e instanceof ReferenceError){
+        alert("Error en direccionar");
+      }
+    }
+  });
 });
 
 function Menu(value){
