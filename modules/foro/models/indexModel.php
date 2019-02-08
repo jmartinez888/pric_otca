12eace2e7f2197c1cca6ab924d78ca66840f2fcb
Phyroz20 <42198072+Phyroz20@users.noreply.github.com>
2019-02-07 21:13:57 -0500

    Update indexModel.php

--- modules/foro/models/indexModel.php
+++ modules/foro/models/indexModel.php
@@ -253,7 +253,7 @@ class indexModel extends Model {
     public function getLineaTematica($Lit_IdLineaTematica, $Idi_IdIdioma="es") {
         try {
             $post = $this->_db->query(
-                    "SELECT Lit_Nombre
+                    "SELECT Lit_Nombre, Lit_IdLineaTematica
                         FROM linea_tematica
                         WHERE Lit_IdLineaTematica = {$Lit_IdLineaTematica}");
             return $post->fetch();
