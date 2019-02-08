f1ae44a43dc6233653290feeac7b8b2ab77c2e6c
gantz7774 <40181548+gantz7774@users.noreply.github.com>
2019-02-08 12:51:05 -0500

    update
    getexamenXLeccion

--- modules/elearning/models/examenModel.php
+++ modules/elearning/models/examenModel.php
@@ -47,7 +47,7 @@ class examenModel extends Model {
             e.Exa_Estado,
             e.Row_Estado,
             fn_devolverIdioma('examen',e.Cur_IdCurso,'$Idi_IdIdioma',e.Idi_IdIdioma) Idi_IdIdioma 
-            FROM examen e WHERE Lec_IdLeccion = $Lec_IdLeccion AND Exa_Estado = 1 AND Row_Estado = 1 ";
+            FROM examen e WHERE e.Lec_IdLeccion = $Lec_IdLeccion AND e.Exa_Estado = 1 AND e.Row_Estado = 1 ";
             $result = $this->_db->prepare($sql);
             $result->execute();
             return $result->fetch(PDO::FETCH_ASSOC);
