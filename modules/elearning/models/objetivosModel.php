6f0ba58acb754344804b7fe8cbedd8c475e62ff0
gantz7774 <40181548+gantz7774@users.noreply.github.com>
2019-02-09 13:15:47 -0500

    update getObjs
    traduccion

--- modules/elearning/models/objetivosModel.php
+++ modules/elearning/models/objetivosModel.php
@@ -4,7 +4,15 @@ class objetivosModel extends Model {
 
     public function __construct() { parent::__construct(); }
 
-    public function getObjs($id){
-        return $this->getArray("SELECT * FROM curso_objetivos WHERE Cur_IdCurso = {$id}");
+    public function getObjs($id, $Idi_IdIdioma = "es"){
+        return $this->getArray("SELECT CO_IdObjetivo,
+        Cur_IdCurso,
+        fn_TraducirContenido('curso_objetivos','CO_Titulo',CO_IdObjetivo,'$Idi_IdIdioma',CO_Titulo) CO_Titulo,
+        fn_TraducirContenido('curso_objetivos','CO_Descripcion',CO_IdObjetivo,'$Idi_IdIdioma',CO_Descripcion) CO_Descripcion,
+        CO_FechaReg,
+        CO_Estado,
+        Row_Estado,
+        fn_devolverIdioma('curso_objetivos',CO_IdObjetivo,'$Idi_IdIdioma',Idi_IdIdioma) Idi_IdIdioma
+        FROM curso_objetivos WHERE Cur_IdCurso = {$id}");
     }
 }
