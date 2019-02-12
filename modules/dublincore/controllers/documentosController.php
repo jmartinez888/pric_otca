675d043457c73276757b91e5668690f1611001e0
JoaquinPan23 <joaquinpanduro23@gmail.com>
2019-02-11 19:39:36 -0500

    Update documentosController.php
    llamando a bdlegal para metadata

--- modules/dublincore/controllers/documentosController.php
+++ modules/dublincore/controllers/documentosController.php
@@ -753,8 +753,9 @@ class documentosController extends Controller{
     {
     	// $this->_acl->autenticado();
     $this->_view->getLenguaje("bd_documentos");
-    $this->_view->getLenguaje("bdrecursos_metadata");	    
+    $this->_view->getLenguaje("bdrecursos_metadata");
     $this->_view->getLenguaje("bdrecursos_index");
+    $this->_view->getLenguaje("bdlegal");
     $this->validarUrlIdioma();
     $this->_view->setTemplate(LAYOUT_FRONTEND);
         $this->_view->setCss(array("jp-index"));
